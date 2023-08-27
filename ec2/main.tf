d318a167d558512f2ad5b9ae3005532e0df4c244
saikumarsooda2 <101648703+saikumarsooda2@users.noreply.github.com>
2023-08-27 19:32:46 +0100

    made double quote for vpc security group

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.example.id
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [sg-06f9944ca8edc98f7]
+  vpc_security_group_ids = ["sg-06f9944ca8edc98f7"]
 
   tags = {
     Name = var.name
